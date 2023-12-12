5473bd3c619836154d25f28ffb034e4d61e16cdd
ankkiet <149255132+ankkiet@users.noreply.github.com>
2023-12-12 19:38:07 +0700

    hinh trai tim

---
+++ aibit-1.py
@@ -0,0 +1,3 @@
+def trung_binh_cong(ds):
+    tb_cong = sum(ds) / len(ds)
+    return tb_cong
---
+++ hinh trai tim.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+t.color('red', 'pink')
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.setheading(60)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+turtle.done()                                                                           
